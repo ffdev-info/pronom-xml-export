af29e9eec74a0c629e680de5c9f74d4313cbd0d3
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2013-07-20 16:09:06 +1200

    [refactor] Dynamic handling of PUID types.

--- pronom-xml-export.py
+++ pronom-xml-export.py
@@ -3,45 +3,15 @@ import os
 import time
 import ConfigParser
 
+puid_type_list = ['x-fmt', 'fmt', 'x-cmp', 'cmp', 'x-chr', 'chr', 'x-sfw', 'sfw']
+
 config = ConfigParser.RawConfigParser()
 config.read('pronom-xml-export.cfg')
 
-max_fmt = config.getint('puids', 'fmt')
-max_xfmt = config.getint('puids', 'xfmt')
+puid_dict = {}
 
-max_cmp = config.getint('puids', 'fmt')
-max_xcmp = config.getint('puids', 'xfmt')
-
-max_chr = config.getint('puids', 'fmt')
-max_xchr = config.getint('puids', 'xfmt')
-
-max_sfw = config.getint('puids', 'fmt')
-max_xsfw = config.getint('puids', 'xfmt')
-
-def set_puid_no(puid_type):
-
-	puid_no = 0
-
-	if puid_type == 'fmt':
-		puid_no = max_fmt
-	elif puid_type == 'x-fmt':
-		puid_no = max_xfmt
-	elif puid_type == 'cmp':
-		puid_no = max_cmp
-	elif puid_type == 'x-cmp':
-		puid_no = max_xcmp
-	elif puid_type == 'chr':
-		puid_no = max_chr
-	elif puid_type == 'x-chr':
-		puid_no = max_xfmt
-	elif puid_type == 'sfw':
-		puid_no = max_sfw
-	elif puid_type == 'x-sfw':
-		puid_no = max_xsfw
-	else:
-		puid_no = 0
-		
-	return puid_no
+for type in puid_type_list:
+	puid_dict[type] = config.getint('puids', type)
 
 # BOF strings we expect to see under normal and error conditions
 error_string = '<!DOCTYPE html'
@@ -50,13 +20,11 @@ good_string = '<?xml version='
 #url through which to access Pronom data...
 base_url = 'http://www.nationalarchives.gov.uk/PRONOM/'
 
-puid_type_list = ['x-fmt', 'fmt', 'x-cmp', 'cmp', 'x-chr', 'chr', 'x-sfw', 'sfw']
-
 def export_data():
-	for x in puid_type_list:
-		puid_type_url = base_url + x + '/'
+	for puid_type in puid_type_list:
+		puid_type_url = base_url + puid_type + '/'
 		current_dir = os.getcwd()
-		new_dir = os.getcwd() + '//' + config.get('locations', 'export') + '//' + x + '//'
+		new_dir = os.getcwd() + '//' + config.get('locations', 'export') + '//' + puid_type + '//'
 		
 		try:
 			os.makedirs(new_dir)
@@ -67,15 +35,16 @@ def export_data():
 		os.chdir(new_dir)
 		
 		#urlib code here...
-		puid_no = set_puid_no(x)
+		puid_no = puid_dict[puid_type]
 
-		for i in range(1,puid_no+1):
-			filename = x + str(i) + '.xml'
+		for i in range(1, puid_no + 1):
+			filename = puid_type + str(i) + '.xml'
 			puid_url = puid_type_url + str(i) + '.xml'
 			
 			url = urllib2.urlopen(puid_url)
 			
 			test_string = url.read(14)
+			
 			if(check_record(test_string)):
 				f = open(filename, 'wb')
 				f.write(test_string + url.read())
