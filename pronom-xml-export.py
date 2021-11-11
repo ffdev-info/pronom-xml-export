50fbe10cb1c32f6ef2ce5df604806b821e6be1e2
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-11-11 11:15:48 +0100

    Convert script to Python 3
    This represents a basic conversion to Python 3. I am sure that there
    are other optimizations that can be made.

--- pronom-xml-export.py
+++ pronom-xml-export.py
@@ -1,7 +1,9 @@
-import urllib2
+"""Script to export PRONOM XML from PRONOM."""
+import configparser as ConfigParser
 import os
 import time
-import ConfigParser
+import sys
+import urllib.request as urllib2
 
 puid_type_list = ['x-fmt', 'fmt', 'x-cmp', 'cmp', 'x-chr', 'chr', 'x-sfw', 'sfw']
 
@@ -15,7 +17,6 @@ for type in puid_type_list:
    if tmpint > 0:
       puid_dict[type] = tmpint
 
-error_string=config.get('doctypes', 'error-string')
 good_string=config.get('doctypes', 'good-string')
 
 #url through which to access Pronom data...
@@ -25,8 +26,10 @@ def export_data():
 
    proxy = ''
    
-   #comment this to remove proxy requirement
-   proxy = urllib2.ProxyHandler({'http': 'dummy@host:port'})
+   # Uncomment this to enable use of a proxy, and ensure that the port
+   # number can resolve to an integer, e.g. 'dummy@host:2313'
+   #
+   # proxy = urllib2.ProxyHandler({'http': 'dummy@host:port'})
 
    for puid_type in puid_dict.keys():
       puid_type_url = base_url + puid_type + '/'
@@ -35,8 +38,8 @@ def export_data():
       
       try:
          os.makedirs(new_dir)
-      except OSError as (errno, strerror):
-         print "OS error({0}): {1}".format(errno, strerror)
+      except OSError as err:
+         print(err, file=sys.stderr)
 
       #change to expor directory to store xml...
       os.chdir(new_dir)
@@ -57,38 +60,38 @@ def export_data():
          
          request.add_header('User-Agent', 'exponentialDK-PRONOM-Export/0.0.0')
          
-         print puid_url
+         print(puid_url, file=sys.stderr)
 
          url = opener.open(request)
          
          test_string = url.read(14)
          
-         if(check_record(test_string)):
+         if check_record(test_string):
             f = open(filename, 'wb')
             f.write(test_string + url.read())
             f.close()
+         else:
+            print("Not writing record: {}".format(test_string), file=sys.stderr)
 
       #revert back to original directory...
       os.chdir(current_dir)		
 
 
 def check_record(test_string):
-   return_val = 0;
+   """Ensure that a test_string doesn't equal an error string.
 
-   if test_string == error_string:
-      return_val = 0
-   elif test_string == good_string:
-      return_val = 1
-   else:
-      #some other value discovered, do not allow...
-      return_val = 0
-      
-   return return_val
+   :param test_string:  A snippet of string to compare against an error
+         string.
+   :return: True or False (Boolean)
+   """
+   if test_string.decode("utf8") == good_string:
+      return True
+   return False
 
 #time script execution time roughly...
-t0 = time.clock()
+t0 = time.perf_counter()
 
 export_data()
 
 #print script execution time...
-print 'Execution time: ' + str(time.clock() - t0) + ' seconds.'
+print('Execution time: ' + str(time.perf_counter() - t0) + ' seconds.', file=sys.stderr)
